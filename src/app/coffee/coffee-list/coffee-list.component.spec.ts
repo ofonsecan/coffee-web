575299632f258f883d31819ec6668e788607a9cb
Oscar Fonseca <o.fonsecan@uniandes.edu.co>
2023-04-30 12:45:47 -0500

    Se crean las pruebas para validar la tabla de cafes

--- src/app/coffee/coffee-list/coffee-list.component.spec.ts
+++ src/app/coffee/coffee-list/coffee-list.component.spec.ts
@@ -5,15 +5,20 @@ import { DebugElement } from '@angular/core';
 
 import { CoffeeListComponent } from './coffee-list.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CoffeeService } from '../coffee.service';
+import { Coffee } from '../coffee';
+import { faker } from '@faker-js/faker';
 
 describe('CoffeeListComponent', () => {
   let component: CoffeeListComponent;
   let fixture: ComponentFixture<CoffeeListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientModule],
-      declarations: [ CoffeeListComponent ]
+      declarations: [ CoffeeListComponent ],
+      providers: [ CoffeeService ]
     })
     .compileComponents();
   }));
@@ -21,10 +26,39 @@ describe('CoffeeListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CoffeeListComponent);
     component = fixture.componentInstance;
+
+    for(let i = 0; i < 3; i++) {
+      const coffee = new Coffee(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.datatype.number(),
+        faker.image.imageUrl()
+      );
+      component.coffees.push(coffee);
+    }
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it("Component has a table", () => {
+    expect(debug.query(By.css("tbody")).childNodes.length).toBeGreaterThan(0);
+  });
+
+  it('should have 4 <tr> elements', () => {
+    expect(debug.queryAll(By.css('tr'))).toHaveSize(4)
+  });
+
+  it('should have dd tag with the coffee.id', () => {
+    debug.queryAll(By.css('#id')).forEach((dd, i)=>{
+      expect(dd.nativeElement.textContent).toContain(component.coffees[i].id)
+    });
   });
 });
